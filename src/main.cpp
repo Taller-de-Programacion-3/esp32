2dfcb860b76ef4ad63470e61d9df9214817a658d
Javier <javiferreyra97@gmail.com>
2022-05-10 18:32:34 -0300

    Connect to google and make HTTP request

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,41 @@
 #include <Arduino.h>
+#include <WiFi.h>
+
+const char ssid[] = "telecentro-4093";
+const char key[] = "tele-7146058";
+int status = WL_IDLE_STATUS;
+
+WiFiClient client;
 
 void setup() {
-// write your initialization code here
+    // write your initialization code here
     Serial.begin(115200);
-    pinMode(18, OUTPUT);
-    pinMode(19, OUTPUT);
+
+
+    // attempt to connect to Wifi network:
+    while ( status != WL_CONNECTED) {
+        Serial.print("Attempting to connect to network, SSID: ");
+        Serial.println(ssid);
+        WiFi.begin(ssid, key);
+        status = WiFi.waitForConnectResult();
+
+        // wait 10 seconds for connection:
+        delay(10000);
+    }
+
+    // once you are connected :
+    Serial.println("You're connected to the network");
+
+    if (client.connect("google.com", 80)) {
+        Serial.println("Connected!");
+        client.println("GET /search?q=arduino HTTP/1.0");
+        client.println();
+        auto response = client.readString();
+        Serial.println(response);
+    }
 }
 
 void loop() {
-// write your code here
+    delay(1000);
     Serial.println("Loop");
-//    digitalWrite(18, HIGH);
-    for(uint8_t i = 0; i < 255; i++) {
-        analogWrite(19, i);
-        delay(8);
-    }
-//    delay(500);
-    for(uint8_t i = 255; i > 0; i--) {
-        analogWrite(19, i);
-        delay(8);
-    }
-//    digitalWrite(18, LOW);
-//    delay(500);
-
 }
