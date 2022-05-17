9288acbb31a64aa8fc43d5c2549eb4930edbe8ca
Javier <javiferreyra97@gmail.com>
2022-05-16 22:39:59 -0300

    Fix initialization warning

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,17 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <memory>
 
 const char ssid[] = "telecentro-4093";
 const char key[] = "tele-7146058";
 int status = WL_IDLE_STATUS;
 
-WiFiClient client;
+std::unique_ptr<WiFiClient> client;
 
 void setup() {
     // write your initialization code here
     Serial.begin(115200);
-
+    client.reset(new WiFiClient());
 
     // attempt to connect to Wifi network:
     while ( status != WL_CONNECTED) {
@@ -26,16 +27,18 @@ void setup() {
     // once you are connected :
     Serial.println("You're connected to the network");
 
-    if (client.connect("google.com", 80)) {
+    if (client->connect("google.com", 80)) {
         Serial.println("Connected!");
-        client.println("GET /search?q=arduino HTTP/1.0");
-        client.println();
-        auto response = client.readString();
+        client->println("GET /search?q=arduino HTTP/1.0");
+        client->println();
+        auto response = client->readStringUntil('\n');
         Serial.println(response);
+        client->stop();
     }
 }
 
 void loop() {
     delay(1000);
+
     Serial.println("Loop");
 }
