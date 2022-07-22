f9c73f98595b700b909cb352dd13e71b4c9ffc65
Javier <javiferreyra97@gmail.com>
2022-07-22 16:59:27 -0300

    Implement led tasks

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,35 @@
 #include <Arduino.h>
+#include <cJSON.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
-#include <cJSON.h>
 #include <memory>
+
 const char ssid[] = "telecentro-4093";
 const char key[] = "tele-7146058";
 int status = WL_IDLE_STATUS;
 
+int Led_Red = 18;
+int Led_Green = 19;
+
 std::unique_ptr<HTTPClient> httpClient;
 std::unique_ptr<WiFiClient> wifiClient;
 
+void turnOnLed(uint8_t led) {
+    analogWrite(led, 255);
+}
+void turnOffLed(uint8_t led) {
+    analogWrite(led, 0);
+}
+
 void setup() {
+    // Setup led pins
+    pinMode(Led_Red, OUTPUT);
+    pinMode(Led_Green, OUTPUT);
+
+    turnOnLed(Led_Green);
+    delay(2000);
+    turnOffLed(Led_Green);
+
     // write your initialization code here
     Serial.begin(115200);
 
@@ -35,6 +54,7 @@ void setup() {
 
 void loop() {
     delay(5000);
+
     auto success = httpClient->begin("https://taller3-backend.herokuapp.com/devices/tasks/esp32");
     if (success) {
         Serial.println("Connected!");
@@ -46,16 +66,57 @@ void loop() {
                 auto payload = httpClient->getString();
 
                 // Parse JSON
+
                 auto json = std::unique_ptr<cJSON>(cJSON_Parse(payload.c_str()));
                 auto tasks = cJSON_GetObjectItem(json.get(), "tasks");
 
                 Serial.println("Got tasks:");
 
                 // Iterate over tasks array
-                auto task = tasks->child;
-                while (task) {
+                cJSON* task = nullptr;
+                cJSON_ArrayForEach(task, tasks) {
                     Serial.println(cJSON_Print(task));
-                    task = task->next;
+                    auto taskNameJson = cJSON_GetObjectItemCaseSensitive(task, "task_name");
+                    auto taskIDJson = cJSON_GetObjectItemCaseSensitive(task, "id");
+                    if (!cJSON_IsString(taskNameJson)) {
+                        Serial.println("Invalid task name");
+                        continue;
+                    }
+                    if (!cJSON_IsNumber(taskIDJson)) {
+                        Serial.println("Invalid task ID");
+                        continue;
+                    }
+                    auto taskName = taskNameJson->valuestring;
+                    int resultValue = -1;
+
+                    Serial.printf("Task name %s\n", taskName);
+
+                    if (String(taskName) == String("Led On")) {
+                        Serial.println("Turning led on");
+                        turnOnLed(Led_Red);
+                        resultValue = 1;
+                    }
+
+                    if (String(taskName) == String("Led Off")) {
+                        Serial.println("Turning led off");
+                        turnOffLed(Led_Red);
+                        resultValue = 0;
+                    }
+
+                    if (resultValue >= 0) {
+                        auto taskId = taskIDJson->valueint;
+                        auto taskResult = std::unique_ptr<cJSON>(cJSON_CreateObject());
+
+                        cJSON_AddNumberToObject(taskResult.get(), "id", taskId);
+                        cJSON_AddNumberToObject(taskResult.get(), "value", resultValue);
+
+                        auto taskResultArray = std::unique_ptr<cJSON>(cJSON_CreateArray());
+                        cJSON_AddItemToArray(taskResultArray.get(), taskResult.get());
+
+                        Serial.printf("Sending result: %s\n", cJSON_Print(taskResultArray.get()));
+
+                        httpClient->POST(cJSON_Print(taskResultArray.get()));
+                    }
                 }
 
             } else {
