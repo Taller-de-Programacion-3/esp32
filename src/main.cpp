a6769db5e0bcf38d04e4b4a19d64666af19c166b
Javier <javiferreyra97@gmail.com>
2022-08-02 09:00:19 -0300

    Implement cpu frequency set and improve comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <memory>
+#include <esp32/pm.h>
+#include <esp_pm.h>
+#include <esp_bt.h>
 #include "ControllerServer.hpp"
 #include "TasksResponse.hpp"
 #include "constants.h"
@@ -9,8 +12,6 @@ const char ssid[] = "telecentro-4093";
 const char key[] = "tele-7146058";
 int status = WL_IDLE_STATUS;
 
-std::unique_ptr<WiFiClient> wifiClient;
-
 void turnOnLed(uint8_t led) {
     analogWrite(led, 255);
 }
@@ -18,19 +19,42 @@ void turnOffLed(uint8_t led) {
     analogWrite(led, 0);
 }
 
+void set_cpu_frequency() {
+    // Set CPU frequency to lowest
+    Serial.println("Setting CPU frequency");
+    esp_pm_config_esp32_t config = {
+            .max_freq_mhz=40,
+            .min_freq_mhz=40,
+    };
+    auto res = esp_pm_configure(&config);
+    if (res == ESP_ERR_INVALID_ARG) {
+        Serial.println("Invalid arguments");
+    } else if (res == ESP_ERR_NOT_SUPPORTED) {
+        Serial.println("Config not supported");
+    } else if (res == ESP_OK) {
+        Serial.println("Configured CPU frequency OK");
+    }
+}
+
 void setup() {
+    // Disable bluetooth
+    esp_bt_controller_disable();
+
     // Test led
     pinMode(Led_Red, OUTPUT);
     pinMode(Led_Green, OUTPUT);
 
     turnOnLed(Led_Green);
-    delay(2000);
+    delay(500);
     turnOffLed(Led_Green);
 
-    // Connect to Wi-Fi
+    // Initiate serial port for logging
     Serial.begin(115200);
 
-    wifiClient.reset(new WiFiClient());
+    set_cpu_frequency();
+
+    // Connect to Wi-Fi
+    status = WiFiClass::status();
 
     // attempt to connect to Wifi network:
     while ( status != WL_CONNECTED) {
@@ -42,10 +66,8 @@ void setup() {
         // wait 10 seconds for connection:
         delay(10000);
     }
-
-    // It is connected!
     Serial.println("You're connected to the network");
-
+    // It is connected!
 }
 
 void loop() {
@@ -60,5 +82,5 @@ void loop() {
         task->sendResponse(server);
     }
 
-    delay(5000);
+    sleep(5);
 }
